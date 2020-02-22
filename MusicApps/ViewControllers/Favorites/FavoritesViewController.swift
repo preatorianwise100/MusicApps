579c4ab365302b07dcfbffc35e6c23ec33864cd4
preatorianwise99 <preatorianwise99@hotmail.com>
2020-02-22 16:18:19 -0300

    add tableviews codes

--- MusicApps/ViewControllers/Favorites/FavoritesViewController.swift
+++ MusicApps/ViewControllers/Favorites/FavoritesViewController.swift
@@ -9,11 +9,12 @@
 import Foundation
 import UIKit
 
-class FavoritesViewController: UIViewController {
+class FavoritesViewController: UIViewController,UITableViewDataSource ,UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableViewSetup()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -22,5 +23,25 @@ class FavoritesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableViewSetup()  {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 //viewModel.arrayOfList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? TableViewCellFav else { return UITableViewCell() }
+        
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("deberia abrir VCDetailsPopular")
+        
+        
+    }
 }
--- MusicApps/ViewControllers/Music/MusicViewController.swift
+++ MusicApps/ViewControllers/Music/MusicViewController.swift
@@ -9,18 +9,37 @@
 import Foundation
 import UIKit
 
-class MusicViewController: UIViewController {
+class MusicViewController: UIViewController,UITableViewDataSource ,UITableViewDelegate {
     
+    @IBOutlet weak var searchers: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableViewSetup()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+    func tableViewSetup()  {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 //viewModel.arrayOfList.count
+    }
     
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? TableViewCellMusic else { return UITableViewCell() }
+        
+       
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("deberia abrir VCDetailsPopular")
+
+        
+    }
 }

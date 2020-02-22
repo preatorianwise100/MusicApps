c4f7a356ecbd36c3a6941bf2cd211daddbe1221e
preatorianwise99 <preatorianwise99@hotmail.com>
2020-02-22 15:09:24 -0300

    add tableviews details VC

---
+++ MusicApps/ViewControllers/Music/MusicDetails/MusicDetails.swift
@@ -0,0 +1,26 @@
+//
+//  MusicDetails.swift
+//  MusicApps
+//
+//  Created by Jose David Bustos H on 2/22/20.
+//  Copyright © 2020 Jose David Bustos H. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MusicDetails: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
